e57c2b1243b580c7bd0cd1e5301381e42f8c1919
9gustin <agustinvazquez453@gmail.com>
2024-02-18 20:05:58 -0300

    feat: magic type from blitz

--- src/features/users/hooks/useCurrentUser.ts
+++ src/features/users/hooks/useCurrentUser.ts
@@ -1,7 +1,10 @@
 import { useQuery } from "@blitzjs/rpc"
+import type { PromiseReturnType } from "blitz"
 import getCurrentUser from "src/features/users/queries/getCurrentUser"
 
-export const useCurrentUser = () => {
+export type UserType = PromiseReturnType<typeof getCurrentUser>
+
+export const useCurrentUser = (): UserType => {
   const [user] = useQuery(getCurrentUser, null)
   return user
 }
