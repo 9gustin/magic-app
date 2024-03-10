dafec42db57e2295613773ea52f6e2e22fe5e848
Agustin Vazquez <agustinvazquez453@gmail.com>
2024-03-09 22:12:28 -0300

    improve user not found page

--- src/pages/u/[username].page.tsx
+++ src/pages/u/[username].page.tsx
@@ -3,20 +3,31 @@ import Layout from "../components/Layout"
 import { useCurrentUser } from "@/features/users/hooks/useCurrentUser"
 import { useQuery } from "@blitzjs/rpc"
 import getUserProfile from "@/features/users/queries/getUserProfile"
-import { BlitzPage, useParam } from "@blitzjs/next"
+import { BlitzPage, Routes, useParam } from "@blitzjs/next"
 import { UserHeader } from "./components/UserHeader"
 import { EditUserModal } from "./components/EditUserModal"
 import { useDisclosure } from "@mantine/hooks"
+import { useRouter } from "next/router"
+import { NotFound } from "../components/NotFound"
 
 const Profile: BlitzPage = () => {
   const username = useParam("username")
+  const router = useRouter()
   const [opened, { open, close }] = useDisclosure(false)
 
   const user = useCurrentUser()
-  const [profileUser] = useQuery(getUserProfile, { username: username?.toString() })
+  const [profileUser, { isFetching }] = useQuery(getUserProfile, { username: username?.toString() })
 
-  if (!username || !profileUser) {
+  if (isFetching) {
     return <Loader />
+  }
+
+  if (!profileUser) {
+    return (
+      <Layout>
+        <NotFound title="User not found" subtitle="" />
+      </Layout>
+    )
   }
 
   return (
