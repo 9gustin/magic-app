a27d126e67e3a5871ebf72af4d9675bd374119cd
Agustin Vazquez <agustinvazquez453@gmail.com>
2024-03-17 12:59:44 -0300

    change primary colors

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -4,5 +4,5 @@ export const defaultTheme: MantineThemeOverride = {
   activeStyles: { transform: "scale(0.95)" },
   defaultRadius: "md",
   cursorType: "pointer",
-  primaryColor: "indigo",
+  primaryColor: "yellow",
 }
--- src/pages/components/AppHeader/AppHeader.tsx
+++ src/pages/components/AppHeader/AppHeader.tsx
@@ -1,6 +1,6 @@
 import { Routes } from "@blitzjs/next"
 import styled from "@emotion/styled"
-import { Flex, Header, Title } from "@mantine/core"
+import { Flex, Header, Title, useMantineTheme } from "@mantine/core"
 import { IconCrystalBall } from "@tabler/icons-react"
 import Link from "next/link"
 import { HeaderMenu } from "./components/Menu"
@@ -12,11 +12,13 @@ const SXHeader = styled(Header)`
 `
 
 export const AppHeader = () => {
+  const theme = useMantineTheme()
+
   return (
     <SXHeader height={60} p="xs">
       <Link href={Routes.Home()}>
         <Flex align="center" gap="sm">
-          <IconCrystalBall />
+          <IconCrystalBall color={theme.colors[theme.primaryColor]?.[4]} />
           <Title size="sm">Magic app</Title>
         </Flex>
       </Link>
